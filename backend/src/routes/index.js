f2753b02407e449d189c04f3d50f0965fe2537ef
Grace Aliko <gracealiko09@gmail.com>
2024-05-12 05:54:02 +0000

    implemented otp verification

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,7 +4,8 @@ const router = express.Router();
 
 
 const userRoutes = require("./../domains/user")
+const OTPRoutes= require("./../domains/otp");
 
 router.use("/user",userRoutes);
-
+router.use("/otp", OTPRoutes);
 module.exports=router;
